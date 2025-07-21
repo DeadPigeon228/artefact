b4098876632dea8b5e88f4183b52e36bfbf0ba27
DeadPigeon228 <deadpigeon28@gmail.com>
2025-07-21 04:21:18 +0300

    Create artifacts.js

---
+++ artifacts.js
@@ -0,0 +1,10 @@
+const artifacts = [
+  {
+    id: 1,
+    name: "Артефакт 1",
+    imageUrl: "https://sun9-47.userapi.com/s/v1/ig2/JC3570oSpPL1HYoIDBplPIMTyVRJhO6Pp6NgT9h8HOg8FQo8v3BxgZDaXwaZSX6TVZhMJXkBs-bP3YI6gXY2uwX1.jpg?quality=95&as=32x33,48x49,72x73,99x101&from=bu&cs=99x0"
+  },
+  // ... остальные артефакты
+];
+
+export default artifacts;
