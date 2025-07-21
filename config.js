3097845e994bd68c06c7e3ef61782b710da140b9
DeadPigeon228 <deadpigeon28@gmail.com>
2025-07-21 04:21:53 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+export const config = {
+  GITHUB_USERNAME: 'DeadPigeon228',
+  GITHUB_REPO: 'artefact',
+  GITHUB_TOKEN: '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', // Замените на свой токен!
+  GITHUB_FILE: 'data.json',
+  STORAGE_KEY: 'playersArtifactsSystem_v10',
+  GENERAL_NOTES_KEY: 'generalNotes_v6'
+};
